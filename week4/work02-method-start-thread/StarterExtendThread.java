e09c96779335fc001da8f4f09a0b471a62ddb473
Golovin Elics <1163854528@qq.com>
2021-11-28 21:10:07 +0800

    feat 第四周-作业二-线程启动方式
    思考有多少种方式，在 main 函数启动一个新线程，运行一个方法，拿到这
    个方法的返回值后，退出主线程?

---
+++ week4/work02-method-start-thread/StarterExtendThread.java
@@ -0,0 +1,42 @@
+/**
+ * 本周作业：（必做）思考有多少种方式，在 main() 函数启动一个新线程或线程池，
+ * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
+ * 写出你的方法，越多越好，提交到 github。
+ */
+
+/**
+ * @author liangfeng
+ * @version 1.0
+ */
+class ExtendThread extends Thread {
+
+    private static final int x = 20;
+    private static final int INITIAL_VALUE = 66;
+    private static final int INITIAL_VALUE_TIME = 10;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < INITIAL_VALUE_TIME; i++) {
+            System.out.println("播放音乐"+ i + "<" + sum() + ">");
+        }
+    }
+
+    private static int sum() {
+        return calculate(x);
+    }
+
+    private static int calculate(int i) {
+        if (i < INITIAL_VALUE) {
+            return 0;
+        } else {
+            return calculate(i - 2) + calculate(i + 1);
+        }
+    }
+}
+
+class ExtendThreadStarter {
+    public static void main(String[] args) {
+        ExtendThread music = new ExtendThread();
+        music.start();
+    }
+}
